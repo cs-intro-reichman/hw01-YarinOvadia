fc5ef302c7b51b6fbd163b670d815cb3763f1d34
Yarin Ovadia <yarin.ovadia@post.runi.ac.il>
2023-12-21 14:49:07 +0200

    blabla

--- GenThree.java
+++ GenThree.java
@@ -1,16 +1,26 @@
 public class GenThree {
+
+
+
     public static void main(String[] args) {
-       
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
-      //  double range = (b-a) + 1
-// 
-        System.out.println(int rand1 = (int) (Math.random() * (b - a)) + a);
-        System.out.println(int rand2 = (int) (Math.random() * (b - a)) + a);
-        System.out.println(int rand3 = (int) (Math.random() * (b - a)) + a);
+      //  double range 
+//
+        int rand1 = (int) (Math.random() * (b - a)) + a;
+        int rand2 = (int) (Math.random() * (b - a)) + a;
+        int rand3 = (int) (Math.random() * (b - a)) + a;
+        System.out.println(rand1);
+        System.out.println(rand2);
+        System.out.println(rand3);
         
         int min = Math.min(rand1, Math.min(rand2, rand3));
-
         System.out.println("The minimal generated number was " + min);
+
+
+
     }
+
+
+
 }
